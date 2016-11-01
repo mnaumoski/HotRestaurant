72d6590c428934bb6d21b5f7cfbdfdbf579abb50
mnaumoski <naumoski.marija@gmail.com>
2016-10-31 22:03:16 -0400

    router added

---
+++ server.js
@@ -0,0 +1,34 @@
+//Dependencies
+//===================================
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+//Setup Express App
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+//Sets up Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+
+//Listen when serves is running
+app.listen(PORT, function(){
+  console.log("App listens on port " + PORT);
+});
+
+// routes
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, 'home.html'));
+}); 
+
+app.get('/reservation', function(req, res){
+  res.sendFile(path.join(__dirname, 'reservation.html'));
+})
+
+app.get('/view', function(req, res){
+  res.sendFile(path.join(__dirname, 'view.html'));
+})
